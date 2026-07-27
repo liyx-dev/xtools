deed2a4c02c686d3539f9c1eb932a548e19f6cd2
liyx-dev <goddayojoka22@gmail.com>
2026-07-27 17:10:22 +0100

    Add XtoolsIcons object with icon constants

---
+++ src/app/design/XtoolsIcons.kt
@@ -0,0 +1,17 @@
+package com.liyx.xtools.design
+
+object XtoolsIcons {
+
+    const val Voice = "mic"
+
+    const val Download = "download"
+
+    const val Play = "play"
+
+    const val Pause = "pause"
+
+    const val Stop = "stop"
+
+    const val Settings = "settings"
+
+}
