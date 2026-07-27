385841de92d21c7c2e584ec74fc25739c115e6f7
liyx-dev <goddayojoka22@gmail.com>
2026-07-27 17:27:24 +0100

    Add XProgress class for progress indication

---
+++ src/design/components/XProgress.kt
@@ -0,0 +1,14 @@
+package com.liyx.xtools.design.components
+
+/**
+ * Progress indicator.
+ */
+class XProgress(
+
+    var progress: Float = 0f,
+
+    var title: String = "",
+
+    var message: String = ""
+
+)
