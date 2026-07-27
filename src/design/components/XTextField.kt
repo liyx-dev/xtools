d260a87610fcb75310881c7148216d71a6150a0c
liyx-dev <goddayojoka22@gmail.com>
2026-07-27 17:26:18 +0100

    Add reusable XTextField component

---
+++ src/design/components/XTextField.kt
@@ -0,0 +1,16 @@
+package com.liyx.xtools.design.components
+
+/**
+ * Reusable text field.
+ */
+class XTextField(
+
+    var label: String,
+
+    var value: String = "",
+
+    var hint: String = "",
+
+    var maxCharacters: Int = Int.MAX_VALUE
+
+)
