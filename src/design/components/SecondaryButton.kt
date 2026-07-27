9615298a03d7fa75c0401cf16599f9f507e447d9
liyx-dev <goddayojoka22@gmail.com>
2026-07-27 17:25:11 +0100

    Add SecondaryButton class for UI components

---
+++ src/design/components/SecondaryButton.kt
@@ -0,0 +1,17 @@
+package com.liyx.xtools.design.components
+
+/**
+ * Secondary Button
+ *
+ * Example:
+ * Cancel
+ * Preview
+ * Share
+ */
+class SecondaryButton(
+
+    var text: String,
+
+    var enabled: Boolean = true
+
+)
