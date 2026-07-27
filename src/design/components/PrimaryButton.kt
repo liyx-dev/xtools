06a5d7bbd05e2e08c0c33e0ae0b7307c60185ecf
liyx-dev <goddayojoka22@gmail.com>
2026-07-27 17:24:26 +0100

    Add PrimaryButton component with properties

---
+++ src/design/components/PrimaryButton.kt
@@ -0,0 +1,19 @@
+package com.liyx.xtools.design.components
+
+/**
+ * Primary CTA Button
+ *
+ * Example:
+ * Generate Voice
+ * Download Audio
+ * Continue
+ */
+class PrimaryButton(
+
+    var text: String,
+
+    var enabled: Boolean = true,
+
+    var loading: Boolean = false
+
+)
