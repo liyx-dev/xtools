2e2a1386f5351d16b2af88bc18968e5febc9f31e
liyx-dev <goddayojoka22@gmail.com>
2026-07-27 17:29:03 +0100

    Add LoadingView class for displaying loading state

---
+++ src/design/components/LoadingView.kt
@@ -0,0 +1,14 @@
+package com.liyx.xtools.design.components
+
+/**
+ * Loading View.
+ */
+class LoadingView(
+
+    var title: String,
+
+    var message: String,
+
+    var progress: Float = 0f
+
+)
