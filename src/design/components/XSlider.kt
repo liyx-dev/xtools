4887ce1e22ee37c36c785e7025426f47100ebffd
liyx-dev <goddayojoka22@gmail.com>
2026-07-27 17:26:48 +0100

    Add XSlider class for premium slider component

---
+++ src/design/components/XSlider.kt
@@ -0,0 +1,16 @@
+package com.liyx.xtools.design.components
+
+/**
+ * Premium slider.
+ */
+class XSlider(
+
+    var label: String,
+
+    var value: Float = 1f,
+
+    var min: Float = 0.5f,
+
+    var max: Float = 2f
+
+)
