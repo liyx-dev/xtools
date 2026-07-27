32091b987c013d28d5dcced4c8d17771010b602e
liyx-dev <goddayojoka22@gmail.com>
2026-07-27 16:40:26 +0100

    Create MainActivity for Xtools application
    Added MainActivity as the entry point for Xtools.

---
+++ src/app/ui/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.liyx.xtools.app.ui
+
+import android.app.Activity
+import android.os.Bundle
+
+/**
+ * MainActivity
+ *
+ * Entry point of Xtools.
+ */
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+
+        // TODO:
+        // Load HomeScreen
+
+    }
+
+}
