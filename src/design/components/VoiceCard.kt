b9adbb0929667dcdf120d1fbe50541a7821ca668
liyx-dev <goddayojoka22@gmail.com>
2026-07-27 17:29:52 +0100

    Add VoiceCard class for voice selection

---
+++ src/design/components/VoiceCard.kt
@@ -0,0 +1,14 @@
+package com.liyx.xtools.design.components
+
+/**
+ * Voice selection card.
+ */
+class VoiceCard(
+
+    var voiceName: String,
+
+    var language: String,
+
+    var selected: Boolean = false
+
+)
