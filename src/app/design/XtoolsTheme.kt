e51441b187b3de1d16a514473890ff12680747dd
liyx-dev <goddayojoka22@gmail.com>
2026-07-27 17:11:43 +0100

    Create XtoolsTheme for design language
    Added central design language for Xtools.

---
+++ src/app/design/XtoolsTheme.kt
@@ -0,0 +1,21 @@
+package com.liyx.xtools.design
+
+/**
+ * Xtools Theme
+ *
+ * Central design language.
+ */
+object XtoolsTheme {
+
+    val Colors = XtoolsColors
+
+    val Shapes = XtoolsShapes
+
+    val Typography = XtoolsTypography
+
+    val Spacing = XtoolsSpacing
+
+    val Animations = XtoolsAnimations
+
+}
+
