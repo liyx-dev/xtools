23a2faa36e23035f21c53c06f3a6187786552987
liyx-dev <goddayojoka22@gmail.com>
2026-07-27 17:25:46 +0100

    Create XCard class for premium card component

---
+++ src/design/components/XCard.kt
@@ -0,0 +1,14 @@
+package com.liyx.xtools.design.components
+
+/**
+ * Premium Card
+ */
+class XCard(
+
+    var title: String,
+
+    var subtitle: String = "",
+
+    var elevated: Boolean = true
+
+)
