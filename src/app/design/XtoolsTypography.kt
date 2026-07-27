0c2b46bafe32afae515fa69679a4bee07e49370f
liyx-dev <goddayojoka22@gmail.com>
2026-07-27 17:09:20 +0100

    Add XtoolsTypography object for font weights

---
+++ src/app/design/XtoolsTypography.kt
@@ -0,0 +1,18 @@
+package com.liyx.xtools.design
+
+import androidx.compose.ui.text.font.FontWeight
+
+object XtoolsTypography {
+
+    val Light = FontWeight.Light
+
+    val Normal = FontWeight.Normal
+
+    val Medium = FontWeight.Medium
+
+    val SemiBold = FontWeight.SemiBold
+
+    val Bold = FontWeight.Bold
+
+}
+
