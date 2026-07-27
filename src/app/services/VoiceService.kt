0df811ab285b165b5154b0bd15293e19fd9e647f
liyx-dev <goddayojoka22@gmail.com>
2026-07-27 16:45:51 +0100

    Create VoiceService for background voice generation
    Added a new VoiceService class for background voice generation.

---
+++ src/app/services/VoiceService.kt
@@ -0,0 +1,10 @@
+package com.liyx.xtools.app.services
+
+/**
+ * Background voice generation service.
+ *
+ * Future versions will allow
+ * generation while the app
+ * stays in the background.
+ */
+class VoiceService
