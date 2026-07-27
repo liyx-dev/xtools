ddc64de055a5d95e45406a648d3dc89026e84a16
liyx-dev <goddayojoka22@gmail.com>
2026-07-27 16:41:30 +0100

    Create HomeScreen class with openVoiceStudio method
    Initial implementation of HomeScreen class with a method to open Voice Studio.

---
+++ src/app/ui/HomeScreen.kt
@@ -0,0 +1,16 @@
+package com.liyx.xtools.app.ui
+
+/**
+ * HomeScreen
+ *
+ * First screen shown to users.
+ */
+class HomeScreen {
+
+    fun openVoiceStudio() {
+
+        // Navigate to Voice Studio
+
+    }
+
+}
