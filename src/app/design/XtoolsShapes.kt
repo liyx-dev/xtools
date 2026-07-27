f7c01cd1499a316efee06db62517009c40c0dece
liyx-dev <goddayojoka22@gmail.com>
2026-07-27 17:08:34 +0100

    Add XtoolsShapes object for rounded corner shapes

---
+++ src/app/design/XtoolsShapes.kt
@@ -0,0 +1,16 @@
+package com.liyx.xtools.design
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.unit.dp
+
+object XtoolsShapes {
+
+    val Small = RoundedCornerShape(12.dp)
+
+    val Medium = RoundedCornerShape(18.dp)
+
+    val Large = RoundedCornerShape(24.dp)
+
+    val Pill = RoundedCornerShape(100.dp)
+
+}
