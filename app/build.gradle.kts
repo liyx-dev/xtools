c784b9021e40f6c0f2f3008a5f73e43f2216b477
liyx-dev <goddayojoka22@gmail.com>
2026-07-28 09:04:52 +0100

    Fix Material3 theme configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,7 +63,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
 
     implementation("androidx.compose.material3:material3")
-
+implementation("com.google.android.material:material:1.12.0")
     debugImplementation("androidx.compose.ui:ui-tooling")
 implementation("androidx.navigation:navigation-compose:2.9.3")
 
