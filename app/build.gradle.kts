16a52bbaa1da4d345728e53d0d06b1b4af21ccda
liyx-dev <goddayojoka22@gmail.com>
2026-08-07 12:55:59 +0100

    Added Onnx Runtime

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,7 @@ implementation("com.google.android.material:material:1.12.0")
 implementation("androidx.navigation:navigation-compose:2.9.3")
 
 implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.2")
+implementation("com.microsoft.onnxruntime:onnxruntime-android:1.22.0")
 
 implementation("androidx.compose.material:material-icons-extended")
 }
